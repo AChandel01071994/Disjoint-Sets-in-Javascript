bac6171809fa9e7bc503eff17009d20cd790bb75
AChandel01071994 <31288986+AChandel01071994@users.noreply.github.com>
2019-07-08 20:39:19 +0530

    bug fixed

--- disjoint-sets.js
+++ disjoint-sets.js
@@ -1,4 +1,4 @@
-// Disjoint set data structure in javascript using Map & Set
+// Disjoint set data structure in javascript
 {
     class DisJointSets {
         store = new Map();
@@ -9,42 +9,31 @@
         }
 
         union(a, b){
-            let setA, setB;
-            // find a & b and fetch their host sets
+            let keyA, keyB;
+            // find a & b and fetch their host keys
             for(let key of this.store.keys()){
-                if(this.store.get(key).has(a)) setA = this.store.get(key);
-                if(this.store.get(key).has(b)) setB = this.store.get(key);
+                if(this.store.get(key).has(a)) keyA = key;
+                if(this.store.get(key).has(b)) keyB = key;
             }
-            if(!setA || !setB || setA === setB) return null;
+
+            if(!keyA || !keyB || keyA === keyB) return null;
 
             // create new set containing items from both sets
-            this.store.set(a, new Set([...setA, ...setB]));
+            this.store.set(keyA, new Set([...this.store.get(keyA), ... this.store.get(keyB)]));
 
-            // delete set b (no need to delete a since we have updated it)
-            this.store.delete(b);
-
+            // delete keyB (no need to delete keyA since we have updated it)
+            this.store.delete(keyB);
             return this.store;
-             
         }
 
         checkSet(a, b){
-            // check if both items exists in same set
+             // check if both items exists in same set
             for(let key of this.store.keys()){
                 let set = this.store.get(key);
                 if(set.has(a) && set.has(b)){
-                    return true;
+                    return true; 
                 }
             }
             return false;
         }
     }
-
-    let dset = new DisJointSets();
-    dset.create(1)
-    dset.create(2)
-    dset.create(3)
-    dset.create(4)
-    dset.union(1,2)
-    dset.union(4,1)
-    dset.checkSet(1,3)
-}
